9da4ba903da5453b9ef33a73d0356f3467f904a8
Alphasquare1 <yashXcodes@gmail.com>
2024-09-06 23:28:15 +0530

    changes made on 6th sep 2024

--- src/string/s.java
+++ src/string/s.java
@@ -2,9 +2,13 @@ package string;
 
 public class s {
     public static void main(String[] args) {
-        String a= new String("yash");
-        String b =new String("yash");
+        String a="yash";
+        String b =a;
+
         System.out.println(a==b);
+        String c= new String("yash");
+        String d =new String("yash");
+        System.out.println(c==d);
     }
     
 }
