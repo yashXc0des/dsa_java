19fbb3292c307606593375aba389606a926d987e
Alphasquare1 <yashXcodes@gmail.com>
2024-09-15 14:56:25 +0530

    15 september 2024

--- src/bitwise/magicmumber.java
+++ src/bitwise/magicmumber.java
@@ -1,23 +1,21 @@
 package bitwise;
 
+import java.util.Arrays;
+
 public class magicmumber {
     public static int nthMagicnumber(int n){
             StringBuilder binaryRepresentation = new StringBuilder();
             int a = n ;
-            
             // Convert the number to its binary representation
             while (a > 0) {
                 int remainder = a % 2;      // Get the remainder (0 or 1)
                 binaryRepresentation.append(remainder); // Append the remainder to the binary string
                 a /= 2; // Divide by 2 for the next bit
             }
-            
             // The binary representation is in reverse order
             binaryRepresentation.reverse();
-            
             // Convert binary string to integer
             int result = Integer.parseInt(binaryRepresentation.toString(), 2);
-            
             return result;
         }
             public static int uniquenumber(int[] arr, int n){
@@ -50,7 +48,6 @@ public class magicmumber {
                 for (int i = 0; i < binaryNumber.length(); i++) {
                     // Convert character to integer (0 or 1)
                     int bit = binaryNumber.charAt(i) - '0';  // Converts char to int (0 or 1)
-                    
                     // Add to the magical number if the bit is 1
                     if (bit == 1) {
                         magicalNumber += Math.pow(5, length - i );  // Use the correct power of 5
@@ -62,7 +59,7 @@ public class magicmumber {
 
             public static int magicnumberbitwise(int n){
                 int magicnumber=0;
-                for(int i=0;i<8;i++){
+                for(int i=0;i<32;i++){
                     int num= (n>>i)&1;
                     if(num==1){
                         magicnumber+=Math.pow(5, i+1);
@@ -70,18 +67,57 @@ public class magicmumber {
                 }
                 return magicnumber;
             }
-
-
-        
+            public static boolean isPowerOf2(int n){
+                int totalbits=0;
+                for(int i =0;i<32;i++){
+                    int num =(n>>i)&1;
+                    if(num==1){
+                        totalbits++;
+                    }
+                }
+                if(totalbits==1){
+                    return true;
+                }
+                return false;
+            }
+            public static int powerofnumber(int n , int b){
+                int result=1;
+                for(int i=0;i<32;i++){
+                    int  num = (b>>i)&1;
+                    if(num==1){
+                        result*=Math.pow(n, Math.pow(2, i));
+                    }
+                }
+                return result;
+            }
         public static void main(String[] args) {
-        //System.out.println(nthMagicnumber(4));
-          // Example array where every number appears 3 times except one number
-          int[] arr = {5, 3, 5, 3, 5, 3, 7};
-          int n = 3; // All numbers except one appear 3 times
-          
-          int uniqueNumber = uniquenumber(arr, n);
-        //   System.out.println(uniqueNumber);
-        System.out.println(nthMagicNumberNonBitwise(3));
-        System.out.println(magicnumberbitwise(3));
+            int[][] image={{1,1,0},{1,0,1},{0,0,0}};
+            flipAndInvertImage(image);
+            System.out.println(Arrays.deepToString(image));
+            
     }
+
+        public static int[][] flipAndInvertImage(int[][] image) {
+            for(int i=0;i<image.length;i++){
+                int length=image[i].length;
+                for(int j=0;j<image[i].length/2;j++){
+                    if(image[i][j]!=image[i][length-1]){
+                        int tamp=image[i][j];
+                        image[i][j]= image[i][length-1];
+                        image[i][length-1]=tamp;
+                    }
+                    length--;
+
+                }
+                for(int a=0;a<image[i].length;a++){
+                    if(image[i][a]==0){
+                        image[i][a]=1;
+                    }else{
+                        image[i][a]=0;
+                    }
+                }
+            }
+            return image;
+        }
+    
 }
