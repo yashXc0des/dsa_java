da7b1d1d678e219fc7b2b97b9b6e701f16338a20
Alphasquare1 <yashXcodes@gmail.com>
2024-09-24 10:16:08 +0530

    asaws

--- src/sorting.java
+++ src/sorting.java
@@ -34,11 +34,53 @@ public class sorting {
             }
         }
     }
+    public static int[] mergeSort(int[] arr){
+        if(arr.length==1){
+            return arr;
+        }
+        int mid = arr.length/2;
+        int[] left= Arrays.copyOfRange(arr, 0,mid);
+        int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+        return merge(left,right);
+    }
     // bubble sort i
+
+    private static int[] merge(int[] left, int[] right) {
+    int i=0;
+    int j=0;
+    int k=0;
+    int[] mix= new int[left.length+right.length];
+    while(i<left.length&&j<right.length){
+        if(left[i]<right[j]){
+            mix[k]=left[i];
+            i++;
+        }
+        else{
+            mix[k]=right[j];
+            j++;
+        }
+        k++;
+    }
+    while (i<left.length) {
+        mix[k]=left[i];
+        i++;
+        k++;
+    }
+    while (j<right.length) {
+        mix[k]=right[j];
+        j++;
+        k++;
+    }
+    return mix;        
+    
+    }
 
     public static void main(String[] args) {
         int[] a = {5, 4, 3, 2, 1};
-        cyclicSort(a);
+        //cyclicSort(a);
+        int[] b = mergeSort(a);
+        System.out.println(Arrays.toString(b));
+        System.out.println(Arrays.toString(a));
 
         System.out.println(Arrays.toString(a)); // Should output [1, 2, 3, 4, 5]
     }
